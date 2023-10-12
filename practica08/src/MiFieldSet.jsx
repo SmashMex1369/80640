5fa57aed5eeb4c5fe31cfa8e4101870e1a420c57
SmashMex1369 <chris1203aless@gmail.com>
2023-10-12 13:03:03 -0600

    practica08

---
+++ practica08/src/MiFieldSet.jsx
@@ -0,0 +1,16 @@
+function MyFieldSet(props){
+    const {titulo, txt1, txt2} = props
+    return (
+        <>
+            <fieldSet>
+                <legend>{titulo}</legend>
+                <label htmlFor={txt1}>{txt1}</label>
+                <input type="text" id={txt1}/>
+                <label htmlFor={txt2}>{txt2}</label>
+                <input type="text" id={txt2}/>
+            </fieldSet> 
+        </>
+    )
+}
+
+export default MyFieldSet
