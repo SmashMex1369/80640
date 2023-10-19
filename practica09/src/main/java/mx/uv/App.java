9cae1420fb1f9fd8a756091940680dba325f0e27
SmashMex1369 <chris1203aless@gmail.com>
2023-10-19 12:55:06 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,5 +1,6 @@
 package mx.uv;
 
+import static spark.Spark.*;
 /**
  * Hello world!
  *
@@ -8,6 +9,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola mundo!" );
+        get("/",
+            (request, response)->"<h1>Hello World!</h1>"
+        );
+        get("/ruta1",
+            (request, response)->"<h1>Hola mundo!</h1>"
+        );
+        get("/ruuat2",
+            (request, response)->"<h1>Adios Mundp!</h1>"
+        );
+        get("/ruta3",
+             (request, response)->"adios mundo"
+        );
     }
 }
