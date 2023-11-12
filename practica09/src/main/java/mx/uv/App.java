13e798190328e8284c6fe2edf1ef99fb3c60cc61
SmashMex1369 <chris1203aless@gmail.com>
2023-11-11 18:57:23 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -9,6 +9,33 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        options("/*",(request,response)->{
+
+        String accessControlRequestHeaders=request.headers("Access-Control-Request-Headers");
+
+        if(accessControlRequestHeaders!=null){
+
+        response.header("Access-Control-Allow-Headers",accessControlRequestHeaders);
+
+        }
+
+        String accessControlRequestMethod=request.headers("Access-Control-Request-Method");
+
+        if(accessControlRequestMethod!=null){
+
+        response.header("Access-Control-Allow-Methods",accessControlRequestMethod);
+
+        }
+
+        return "OK";
+
+        });
+
+        before((request,response)->response.header("Access-Control-Allow-Origin","*"));
+
+
+
         get("/",
             (request, response)->"<h1>Hello World!</h1>"
         );
@@ -19,7 +46,7 @@ public class App
             (request, response)->"<h1>Adios Mundp!</h1>"
         );
         get("/ruta3",
-             (request, response)->"adios mundo"
+             (request, response)->"{\"alumno\":\"john\",\"matricula\":\"s0001\",\"carrera\":\"tc\"}"
         );
     }
 }
